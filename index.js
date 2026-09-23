4dbe054f183a0b88375e2efb4157870e68a9e03c
Diego-can <diego.cantarino97@gmail.com>
2026-09-22 22:40:12 -0300

    Ajustes finais e criação da lista de pedidos recentes com javascript

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const sideMenu = document.querySelector("aside");
 const menuBtn = document.getElementById("menu-btn");
 const closeBtn = document.getElementById("close-btn");
 const themeToggle = document.querySelector('.theme-toggle');
+const spans = themeToggle.querySelectorAll('span');
+const today = new Date().toISOString().split('T')[0];
+
+document.getElementById('date-area').value = today;
+
 
 menuBtn.addEventListener('click', () => {
     sideMenu.style.display = 'block';
@@ -13,5 +18,18 @@ closeBtn.addEventListener('click', () => {
 
 themeToggle.addEventListener('click', () => {
     document.body.classList.toggle('dark-theme-variables');
-    themeToggle.querySelector('span').classList.toggle('active');
-})
+    spans.forEach(span => span.classList.toggle('active'));
+});
+
+Orders.forEach(order => {
+    const tr = document.createElement('tr');
+    const trContent = `
+                        <td>${order.productName}</td>
+                        <td>${order.productCode}</td>
+                        <td>${order.paymentStatus}</td>
+                        <td class="${order.shipping === 'Recusado' ? 'danger' : order.shipping === 'Pendente' ? 'warning' : 'primary'}">${order.shipping}</td>
+                        <td class="primary"><a href="#">Detalhes</a></td>
+                        `;
+    tr.innerHTML = trContent;
+    document.querySelector('table tbody').appendChild(tr);
+});
---
+++ orders.js
@@ -0,0 +1,32 @@
+const Orders = [
+    {
+        productName: 'Monitor 32',
+        productCode: 83162,
+        paymentStatus: 'Pago',
+        shipping: 'Pendente'
+    },
+    {
+        productName: 'Teclado',
+        productCode: 74912,
+        paymentStatus: 'Reembolsado',
+        shipping: 'Recusado'
+    },
+    {
+        productName: 'HD Externo 1TB',
+        productCode: 83019,
+        paymentStatus: 'Pago',
+        shipping: 'Enviado'
+    },
+    {
+        productName: 'Mouse Gamer',
+        productCode: 36100,
+        paymentStatus: 'Pago',
+        shipping: 'Pendente'
+    },
+    {
+        productName: 'Controle sem fio',
+        productCode: 19922,
+        paymentStatus: 'Pago',
+        shipping: 'Pendente'
+    }
+]
